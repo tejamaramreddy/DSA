229238ec806a7df990136c2b030d5b474762b059
tejamaramreddy <tejamaramreddy@gmail.com>
2026-01-11 09:25:11 -0600

    rotated binary search

---
+++ arrays/binarysearch/RotatedBinarySearch.java
@@ -0,0 +1,61 @@
+package arrays.binarysearch;
+
+public class RotatedBinarySearch {
+
+    public static int rotatedBinarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Left half sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Right half sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static int rotatedBinarySearch1(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rotatedBinarySearch1(new int[]{4,5,6,0,1,2,3}, 6));
+    }
+}
