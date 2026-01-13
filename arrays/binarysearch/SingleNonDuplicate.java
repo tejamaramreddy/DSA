179a63c59da4b6f7dadc21d4059f15b6ed7f19e3
tejamaramreddy <tejamaramreddy@gmail.com>
2026-01-13 13:51:04 -0600

    finding single non-duplicate number in a sorted array using binary search

---
+++ arrays/binarysearch/SingleNonDuplicate.java
@@ -0,0 +1,23 @@
+package arrays.binarysearch;
+
+public class SingleNonDuplicate {
+    public static int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(mid % 2 == 1){
+                mid--;
+            }
+            if(nums[mid] == nums[mid + 1]){
+                start = mid + 2;
+            }else{
+                end = mid;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        System.out.println(singleNonDuplicate(new int[]{3,3,7,7,10,11,11}));
+    }
+}
